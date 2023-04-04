24f256bc45e124e080e3e17ff53bf8305781f0ac
kimjihyeon-angela <angela9830@naver.com>
2023-04-04 17:15:12 +0900

    2023.04.04.

--- Day01/Day01App/cs09_nullcondition/Program.cs
+++ Day01/Day01App/cs09_nullcondition/Program.cs
@@ -10,6 +10,27 @@ namespace cs09_nullcondition
     {
         static void Main(string[] args)
         {
+            Foo myfoo = null; // Foo myfoo = new Foo();
+                              // myfoo.member = 30;
+
+            /*
+             * int? bar;
+            if(myfoo != null) 
+            {
+                bar = myfoo.member;
+            }
+            else
+            {
+                bar = null;
+            }
+            */
+            int? bar = myfoo?.member; // 위 9줄(주석부분)을 모두 축약시킴
         }
+    }
+
+    class Foo
+    {
+        public int member;
+
     }
 }
